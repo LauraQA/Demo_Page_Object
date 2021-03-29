d667df476b36bf8e82719f2aeac869542a7e6959
Laura.Melkonyan <laura.melkonyan@wolterskluwer.com>
2021-03-29 14:57:54 +0400

    Page_Object

--- test/specs/header.spec.js
+++ test/specs/header.spec.js
@@ -4,7 +4,7 @@ const EC = protractor.ExpectedConditions;
 describe("Home page header", function () {
 
     beforeEach(function() {
-            browser.ignoreSynchronization = true;
+            browser.waitForAngularEnabled(false);
             return browser.manage().window().maximize();
     });
 
@@ -17,17 +17,17 @@ describe("Home page header", function () {
         expect(countOfNavigationButtons).to.be.equal(6);
     });
 
-    // This an example of a bad implementation, don't to it this way EVER!
-    xit("should have 6 menu items (Example of how NOT to do it)", async function () {
-        await browser.get("https://www.epam.com");
-        const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
-        const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
-        const elementToClick = element.all(by.css("div.header .top-navigation__item")).get(elementToClickIndex);
-        await elementToClick.click();
-        const firstNavigationButton = element.all(by.css("div.header .top-navigation__item")).get(0);
-        await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
-        const countOfNavigationButtons = await element.all(by.css("div.header .top-navigation__item")).count();
-        expect(countOfNavigationButtons).to.be.equal(6);
-    });
+    // // This an example of a bad implementation, don't to it this way EVER!
+    // xit("should have 6 menu items (Example of how NOT to do it)", async function () {
+    //     await browser.get("https://www.epam.com");
+    //     const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
+    //     const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
+    //     const elementToClick = element.all(by.css("div.header .top-navigation__item")).get(elementToClickIndex);
+    //     await elementToClick.click();
+    //     const firstNavigationButton = element.all(by.css("div.header .top-navigation__item")).get(0);
+    //     await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
+    //     const countOfNavigationButtons = await element.all(by.css("div.header .top-navigation__item")).count();
+    //     expect(countOfNavigationButtons).to.be.equal(6);
+    // });
 
 });
